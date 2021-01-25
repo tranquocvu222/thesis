dd28a8db48f16942ef25206fe07eb89c77429ffc
unknown <tranquocvu22298@gmail.com>
2021-01-25 13:56:14 +0700

    set up

---
+++ src/main/java/ces/riccico/CesRicciCoBeApplication.java
@@ -0,0 +1,13 @@
+package ces.riccico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesRicciCoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CesRicciCoBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ces/riccico/CesRicciCoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ces.riccico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CesRicciCoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
