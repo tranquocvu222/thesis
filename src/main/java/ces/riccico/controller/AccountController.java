60c06594ce98c5fe6942ff0f594e8d9c2ec0d2fa
sangngodn1996 <sangngodn@gmail.com>
2021-01-27 17:29:54 +0700

    update account

--- src/main/java/ces/riccico/controller/AccountController.java
+++ src/main/java/ces/riccico/controller/AccountController.java
@@ -27,7 +27,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import ces.riccico.models.Accounts;
-import ces.riccico.models.Roles;
 import ces.riccico.service.AccountService;
 
 @CrossOrigin
