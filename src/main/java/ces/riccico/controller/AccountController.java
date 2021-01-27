fae2014af2091584ceaa4459b3e8fc00c903256e
unknown <tranquocvu22298@gmail.com>
2021-01-27 17:08:52 +0700

    fix CORS

--- src/main/java/ces/riccico/controller/AccountController.java
+++ src/main/java/ces/riccico/controller/AccountController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,11 +19,13 @@ public class AccountController {
 	@Autowired
 	private AccountService accountService;
 	
+	@CrossOrigin
 	@PostMapping("/login")
 	public ResponseEntity<?> login (@RequestBody Accounts account){
 		return accountService.login(account);
 	}
 	
+	@CrossOrigin
 	@GetMapping("/allAccounts")
 	public List<Accounts> getAll(){
 		return accountService.getAll();
