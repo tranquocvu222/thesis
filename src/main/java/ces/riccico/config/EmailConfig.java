cad685a3047ad7beecab2ba27cc867c6a7999e59
unknown <tranquocvu22298@gmail.com>
2021-02-22 11:37:00 +0700

    update send mail

--- src/main/java/ces/riccico/config/EmailConfig.java
+++ src/main/java/ces/riccico/config/EmailConfig.java
@@ -13,7 +13,19 @@ public class EmailConfig {
 	@Bean
 	public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
+//        mailSender.setHost("smtp.gmail.com");
+//        mailSender.setPort(587);
+// 
+//        mailSender.setUsername("sangngodn96@gmail.com");
+//        mailSender.setPassword("0903335271");
+// 
+//        Properties props = mailSender.getJavaMailProperties();
+//        props.put("mail.transport.protocol", "smtp");
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.debug", "true");
+        
+        mailSender.setHost("smtp.googlemail.com");
         mailSender.setPort(587);
  
         mailSender.setUsername("sangngodn96@gmail.com");
@@ -23,23 +35,12 @@ public class EmailConfig {
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.smtp.ssl.enable", "false");
+        props.put("mail.smtp.socketFactory.port", "578");
+        props.put("mail.smtp.socketFactory.fallback", "true");
         props.put("mail.debug", "true");
-//        mailSender.setHost("smtp.sparkpostmail.com");
-//        mailSender.setPort(587);
-// 
-//        mailSender.setUsername("facebookmailservice2021@gmail.com");
-//        mailSender.setPassword("0903335271");
-// 
-//        Properties props = mailSender.getJavaMailProperties();
-//        props.put("mail.transport.protocol", "smtp");
-//        props.put("mail.smtp.auth", "true");
-//        props.put("mail.smtp.starttls.enable", "true");
-//        props.put("mail.smtp.starttls.required", "true");
-//        props.put("mail.smtp.ssl.enable", "false");
-//        props.put("mail.smtp.socketFactory.port", "578");
-//        props.put("mail.smtp.socketFactory.fallback", "true");
-//        props.put("mail.debug", "true");
-// 
+ 
         return mailSender;
     }
 }
