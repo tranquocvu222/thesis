b849e026c7612998deee891519c9983e24e04693
unknown <tranquocvu22298@gmail.com>
2021-03-29 14:17:09 +0700

    add page and size to search_filter feature

--- src/main/java/ces/riccico/common/constants/CommonConstants.java
+++ src/main/java/ces/riccico/common/constants/CommonConstants.java
@@ -1,7 +1,9 @@
 package ces.riccico.common.constants;
 
 public class CommonConstants {
-
+	
+	public static final String INVALID_PAGE = "invalid page";
+	
 	public static final String FAIL = "fail";
 
 	public static final String LOGIN_REQUIRED = "you must login";
